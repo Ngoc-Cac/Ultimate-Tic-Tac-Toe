adfe17702e28eab7a3d13b59fe277715bffbb473
Trịnh Ngọc Các <cactrinh5@gmail.com>
2024-12-24 16:48:27 +0700

    refactored code
    moved the GUI part of the game menu into menus.py

---
+++ GUI/menus.py
@@ -0,0 +1,94 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont
+import PyQt6.QtWidgets as QtWidgets
+
+from typing import Callable
+
+INFO_FONT: QFont = QFont()
+INFO_FONT.setPointSize(16)
+
+TRANSPARENT_BG: str = "background-color: transparent"
+
+class NewGameMenu(QtWidgets.QFrame):
+    def __init__(self, start_game_func: Callable[[bool], None],
+                 change_gametype_func: Callable[[], None],
+                 change_gamemode_func: Callable[[], None],
+                 change_turn_func: Callable[[], None],
+                 parent = None) -> None:
+        super().__init__(parent)
+        # new game menu
+        vbox = QtWidgets.QVBoxLayout()
+
+        ## game type choosing zone
+        gametype_hbox = self.init_gametype_zone(change_gametype_func)
+        
+        ## mode choosing zone
+        mode_hbox = self.init_gamemode_zone(change_gamemode_func, change_turn_func)
+
+        ## play button zone
+        play_button = QtWidgets.QPushButton('Play!')
+        play_button.clicked.connect(lambda: start_game_func(True))
+        play_button.setStyleSheet(TRANSPARENT_BG)
+        play_button.setFont(INFO_FONT)
+        play_button.setFixedSize(100, 50)
+
+        vbox.addLayout(gametype_hbox)
+        vbox.addLayout(mode_hbox)
+        vbox.addWidget(play_button, alignment=Qt.AlignmentFlag.AlignHCenter)
+        vbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setLayout(vbox)
+
+    def init_gametype_zone(self, change_gametype_func: Callable[[], None]):
+        gametype_box = QtWidgets.QComboBox()
+        gametype_box.addItems(['Normal', 'Ultimate'])
+        gametype_box.setCurrentIndex(1)
+        gametype_box.setStyleSheet(TRANSPARENT_BG)
+        gametype_box.setFont(INFO_FONT)
+        gametype_box.setFixedSize(115, 50)
+        gametype_box.currentTextChanged.connect(change_gametype_func)
+
+        decorative_label = QtWidgets.QLabel('Game Type:')
+        decorative_label.setStyleSheet(TRANSPARENT_BG)
+        decorative_label.setFont(INFO_FONT)
+        decorative_label.setFixedSize(110, 30)
+        decorative_label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignBottom)
+
+        gametype_hbox = QtWidgets.QHBoxLayout()
+        gametype_hbox.addWidget(decorative_label)
+        gametype_hbox.addWidget(gametype_box)
+        gametype_hbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        return gametype_hbox
+    
+    def init_gamemode_zone(self, change_gamemode_func: Callable[[], None],
+                           change_turn_func: Callable[[], None]):
+        mode_box = QtWidgets.QComboBox()
+        mode_box.addItems(['Human', 'Bot'])
+        mode_box.setStyleSheet(TRANSPARENT_BG)
+        mode_box.setFont(INFO_FONT)
+        mode_box.setFixedSize(115, 50)
+        mode_box.currentTextChanged.connect(change_gamemode_func)
+
+        
+        self.choose_turn_butt = QtWidgets.QPushButton('Bot goes first!')
+        self.choose_turn_butt.setFont(INFO_FONT)
+        self.choose_turn_butt.setHidden(True)
+        self.choose_turn_butt.setFixedSize(200, 50)
+        self.choose_turn_butt.setStyleSheet(TRANSPARENT_BG)
+        self.choose_turn_butt.clicked.connect(change_turn_func)
+
+        decorative_label = QtWidgets.QLabel('Mode:')
+        decorative_label.setStyleSheet(TRANSPARENT_BG)
+        decorative_label.setFont(INFO_FONT)
+        decorative_label.setFixedSize(110, 30)
+        decorative_label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignBottom)
+
+        mode_hbox = QtWidgets.QHBoxLayout()
+        mode_hbox.addWidget(decorative_label)
+        mode_hbox.addWidget(mode_box)
+        mode_hbox.addWidget(self.choose_turn_butt)
+        mode_hbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        return mode_hbox
+    
+class InGameMenu(QtWidgets.QFrame):
+    def __init__(self, parent = None):
+        super().__init__(parent)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt6.QtGui import QFont
 import PyQt6.QtWidgets as QtWidgets
 
 from GUI.tictactoe_board import TicTacToe, UltimateTicTacToe
+from GUI.menus import NewGameMenu, InGameMenu
 from minimax.minimax import find_move
 from minimax.gamestate import EMPTY_CHAR
 
@@ -138,98 +139,22 @@ class Home(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.init_gamezone()
-        self.init_menu()
+        self.init_menus()
 
-    def init_menu(self):
+    def init_menus(self):
         bg_color = "background-color: rgba(0, 0, 0, 170)"
-        transparent_bg = "background-color: transparent"
-        self.overlay_menu = {'new': QtWidgets.QWidget(self),
-                             'in-game': QtWidgets.QWidget(self)}
-        
+        self.overlay_menu = {}
+        self.overlay_menu['new'] = NewGameMenu(self.start_game, self.change_gametype,
+                                               self.change_gamemode, self.change_turn,
+                                               self)
+        self.overlay_menu['in-game'] = InGameMenu()
+        self.overlay_menu['in-game'].setHidden(True)
+
         self.overlay_menu['new'].setStyleSheet(bg_color)
         self.overlay_menu['in-game'].setStyleSheet(bg_color)
-        self.overlay_menu['in-game'].setHidden(True)
 
         self.overlay_menu['new'].setFixedSize(*SCREEN_SIZE)
         self.overlay_menu['in-game'].setFixedSize(*SCREEN_SIZE)
-        
-
-        # new game menu
-        vbox = QtWidgets.QVBoxLayout()
-
-        ## game type choosing zone
-        gametype_hbox = self.init_gametype_zone()
-        
-        ## mode choosing zone
-        mode_hbox = self.init_gamemode_zone()
-
-        ## play button zone
-        play_button = QtWidgets.QPushButton('Play!')
-        play_button.clicked.connect(lambda: self.start_game(new_game=True))
-        play_button.setStyleSheet(transparent_bg)
-        play_button.setFont(INFO_FONT)
-        play_button.setFixedSize(100, 50)
-
-        vbox.addLayout(gametype_hbox)
-        vbox.addLayout(mode_hbox)
-        vbox.addWidget(play_button, alignment=Qt.AlignmentFlag.AlignHCenter)
-        vbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.overlay_menu['new'].setLayout(vbox)
-
-    def init_gametype_zone(self):
-        transparent_bg = "background-color: transparent"
-
-        gametype_box = QtWidgets.QComboBox()
-        gametype_box.addItems(['Normal', 'Ultimate'])
-        gametype_box.setCurrentIndex(1)
-        gametype_box.setStyleSheet(transparent_bg)
-        gametype_box.setFont(INFO_FONT)
-        gametype_box.setFixedSize(115, 50)
-        gametype_box.currentTextChanged.connect(self.change_gametype)
-
-        decorative_label = QtWidgets.QLabel('Game Type:')
-        decorative_label.setStyleSheet(transparent_bg)
-        decorative_label.setFont(INFO_FONT)
-        decorative_label.setFixedSize(110, 30)
-        decorative_label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignBottom)
-
-        gametype_hbox = QtWidgets.QHBoxLayout()
-        gametype_hbox.addWidget(decorative_label)
-        gametype_hbox.addWidget(gametype_box)
-        gametype_hbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        return gametype_hbox
-
-    def init_gamemode_zone(self):
-        transparent_bg = "background-color: transparent"
-
-        mode_box = QtWidgets.QComboBox()
-        mode_box.addItems(['Human', 'Bot'])
-        mode_box.setStyleSheet(transparent_bg)
-        mode_box.setFont(INFO_FONT)
-        mode_box.setFixedSize(115, 50)
-        mode_box.currentTextChanged.connect(self.change_gamemode)
-
-        
-        self.choose_turn_butt = QtWidgets.QPushButton('Bot goes first!')
-        self.choose_turn_butt.setFont(INFO_FONT)
-        self.choose_turn_butt.setHidden(True)
-        self.choose_turn_butt.setFixedSize(200, 50)
-        self.choose_turn_butt.setStyleSheet(transparent_bg)
-        self.choose_turn_butt.clicked.connect(self.change_turn)
-
-        decorative_label = QtWidgets.QLabel('Mode:')
-        decorative_label.setStyleSheet(transparent_bg)
-        decorative_label.setFont(INFO_FONT)
-        decorative_label.setFixedSize(110, 30)
-        decorative_label.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignBottom)
-
-        mode_hbox = QtWidgets.QHBoxLayout()
-        mode_hbox.addWidget(decorative_label)
-        mode_hbox.addWidget(mode_box)
-        mode_hbox.addWidget(self.choose_turn_butt)
-        mode_hbox.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        return mode_hbox
-
 
     def init_gamezone(self) -> None:
         global game
@@ -261,7 +186,7 @@ class Home(QtWidgets.QWidget):
     def change_gamemode(self, text: Literal['Human', 'Bot']):
         global gamemode
         gamemode = text
-        self.choose_turn_butt.setHidden(text == 'Human')
+        self.overlay_menu['new'].choose_turn_butt.setHidden(text == 'Human')
     def change_gametype(self, text: Literal['Normal', 'Ultimate']):
         global gametype
         gametype = text
@@ -275,7 +200,7 @@ class Home(QtWidgets.QWidget):
         global bot_goes_first
         bot_goes_first = not bot_goes_first
         subject = 'Bot' if bot_goes_first else 'Human'
-        self.choose_turn_butt.setText(subject + ' goes first!')
+        self.overlay_menu['new'].choose_turn_butt.setText(subject + ' goes first!')
 
 
     def start_game(self, new_game: bool):
