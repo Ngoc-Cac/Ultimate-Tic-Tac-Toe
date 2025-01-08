dd53e8c1fecf956b2077f2f940fedb623f5d2a66
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-01-08 16:49:05 +0700

    refactor gamestate.py
    refactor score function in gamestate.py

--- minimax/gamestate.py
+++ minimax/gamestate.py
@@ -2,20 +2,55 @@ from copy import deepcopy
 
 from typing import TypeAlias, Union, Literal, Optional
 
+
 PlayerCharacter: TypeAlias = Literal['X', 'O']
-TicTacToeBoard: TypeAlias = list[list[PlayerCharacter]]
+TicTacToeBoard: TypeAlias = list[list[PlayerCharacter | Literal[' ']]]
 
 NumericType: TypeAlias = Union[int, float]
 
 
 EMPTY_CHAR: str = ' '
 MAX_SCORE: int = 1000
+TIE_SCORE: int = 1000
+def get_winner(board: TicTacToeBoard) -> Optional[PlayerCharacter]:
+    main_diag = []
+    sub_diag = []
+    for i, row in enumerate(board):
+        # rows
+        row_alias = set(row)
+        if (len(row_alias) == 1) and\
+           (not EMPTY_CHAR in row_alias) and\
+           (not 'T' in row_alias):
+            return row_alias.pop()
+        # cols
+        col = set(cell[i] for cell in board)
+        if (len(col) == 1) and\
+           (not EMPTY_CHAR in col) and\
+           (not 'T' in col):
+            return col.pop()
+
+        main_diag.append(row[i])
+        sub_diag.append(board[2 - i][i])
+
+    main_diag = set(main_diag)
+    sub_diag = set(sub_diag)
+    if (len(main_diag) == 1) and\
+       (not EMPTY_CHAR in main_diag) and\
+       (not 'T' in main_diag):
+        return main_diag.pop()
+    if (len(sub_diag) == 1) and\
+       (not EMPTY_CHAR in sub_diag) and\
+       (not 'T' in sub_diag):
+        return sub_diag.pop()
+    # tie or game has not ended
+    return None
+
 class GameState():
     
     __slots__ = '_board_state', '_play_char', '_free_cells', '_previous_move', '_max_player'
     def __init__(self, board_state: TicTacToeBoard, player_char: PlayerCharacter,
                  max_player: PlayerCharacter, *,
-                 free_cells: set[tuple[int, int]] = None,
+                 free_cells: Optional[set[tuple[int, int]]] = None,
                  previous_move: Optional[tuple[int, int]] = None):
         # Type check stuff please
 
@@ -47,32 +82,13 @@ class GameState():
                                         free_cells=self._free_cells - {pos},
                                         previous_move=pos))
         return new_states
-
+    
     def calculate_score(self) -> Optional[NumericType]:
-        main_diag = []
-        sub_diag = []
-        for i, row in enumerate(self._board_state):
-            # rows
-            row_alias = set(row)
-            if (not EMPTY_CHAR in row_alias) and (len(row_alias) == 1):
-                return MAX_SCORE if self._max_player in row_alias else -MAX_SCORE
-            # cols
-            col = set(cell[i] for cell in self._board_state)
-            if (not EMPTY_CHAR in col) and (len(col) == 1):
-                return MAX_SCORE if self._max_player in col else -MAX_SCORE
-
-            main_diag.append(row[i])
-            sub_diag.append(self._board_state[2 - i][i])
-
-        main_diag = set(main_diag)
-        sub_diag = set(sub_diag)
-        if (not EMPTY_CHAR in main_diag) and len(main_diag) == 1:
-            return MAX_SCORE if self._max_player in main_diag else -MAX_SCORE
-        if (not EMPTY_CHAR in sub_diag) and len(sub_diag) == 1:
-            return MAX_SCORE if self._max_player in sub_diag else -MAX_SCORE
-        # tie
-        if not len(self._free_cells): return 0
-        return None
+        if (winner := get_winner(self._board_state)):
+            return (1 if winner == self._max_player else -1) * MAX_SCORE
+        else:
+            return TIE_SCORE if len(self._free_cells) == 0 else None
+        
 
     def __str__(self) -> str:
         return '\n'.join('|'.join(row) for row in self._board_state)
