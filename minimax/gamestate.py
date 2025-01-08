2ff04513d9e033a64ef504f3ae4a7fc59b07979b
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-01-08 17:55:38 +0700

    refactored gamestate.py
    removed keyword arguments for internal tree expansion (previous_move, free_cells)
    These are now hidden behind kwargs for sake of function documentation

--- minimax/gamestate.py
+++ minimax/gamestate.py
@@ -47,24 +47,27 @@ def get_winner(board: TicTacToeBoard | UltimateTicTacToeBoard) -> Optional[Playe
     return None
 
 class GameState():
-    
     __slots__ = '_board_state', '_play_char', '_free_cells', '_previous_move', '_max_player'
-    def __init__(self, board_state: TicTacToeBoard, player_char: PlayerCharacter,
-                 max_player: PlayerCharacter, *,
-                 free_cells: Optional[set[tuple[int, int]]] = None,
-                 previous_move: Optional[tuple[int, int]] = None):
+    def __init__(self, board_state: TicTacToeBoard,
+                 player_char: PlayerCharacter,
+                 max_player: PlayerCharacter,
+                 # previous_move: Optional[tuple[int, int]]
+                 # free_cells: Optional[set[tuple[int, int]]]
+                 **kwargs):
         # Type check stuff please
 
         self._board_state = board_state
         self._play_char = player_char
         self._max_player = max_player
-        self._previous_move = previous_move
+        self._previous_move = kwargs['previous_move'] if 'previous_move' in kwargs else None
+
+        free_cells = kwargs['free_cells'] if 'free_cells' in kwargs else None
 
         if free_cells is None:
-            free_cells = set((i, j) for i, row in enumerate(self._board_state)
-                                    for j, cell in enumerate(row)
-                                    if cell == EMPTY_CHAR)
-        self._free_cells = free_cells
+            self._free_cells = set((i, j) for i, row in enumerate(self._board_state)
+                                          for j, cell in enumerate(row)
+                                          if cell == EMPTY_CHAR)
+        else: self._free_cells = free_cells
 
     @property
     def previous_move(self) -> tuple[int, int]:
@@ -100,23 +103,34 @@ class UltimateGameState(GameState):
     def __init__(self, main_board_state: UltimateTicTacToeBoard,
                  subboard_states: list[list[TicTacToeBoard]],
                  player_char: PlayerCharacter,
-                 max_player: PlayerCharacter, *,
-                 free_cells: Optional[set[tuple[int, int, int, int]]] = None,
-                 previous_move: Optional[tuple[int, int, int, int]] = None):
-        self._subboards = subboard_states
+                 max_player: PlayerCharacter,
+                 # board_to_play: Optional[tuple[int, int]] = None
+                 # previous_move: Optional[tuple[int, int, int, int]]
+                 # free_cells: Optional[set[tuple[int, int, int, int]]]
+                 **kwargs):
+        free_cells = kwargs['free_cells'] if 'free_cells' in kwargs else None
+        board_to_play = kwargs['board_to_play'] if 'board_to_play' in kwargs else None
+        previous_move = kwargs['previous_move'] if 'previous_move' in kwargs else None
 
-        if free_cells is None:
-            free_cells = set()
-            for i, row in enumerate(self._subboards):
+        free_cells = set()
+        if board_to_play is None:
+            for i, row in enumerate(subboard_states):
                 for j, board in enumerate(row):
                     if main_board_state[i][j] != EMPTY_CHAR: continue
                     free_cells.update((i, j , k, l)
                                       for k, subrow in enumerate(board)
                                       for l, cell in enumerate(subrow)
                                       if cell == EMPTY_CHAR)
+        else:
+            b2p = subboard_states[board_to_play[0]][board_to_play[1]]
+            free_cells.update((*board_to_play, k, l)
+                              for k, subrow in enumerate(b2p)
+                              for l, cell in enumerate(subrow)
+                              if cell == EMPTY_CHAR)
 
         super().__init__(main_board_state, player_char, max_player,
                          free_cells=free_cells, previous_move=previous_move)
+        self._subboards = subboard_states
 
     def expand_state(self) -> list['UltimateGameState']:
         new_states: list[UltimateGameState] = []
@@ -132,24 +146,14 @@ class UltimateGameState(GameState):
                 if not sum(row.count(EMPTY_CHAR) for row in next_subboards[pos[0]][pos[1]]):
                     next_board[pos[0]][pos[1]] = 'T'
 
-            if next_board[pos[2]][pos[3]] == EMPTY_CHAR:
-                next_free_cells = set((pos[2], pos[3], i, j)
-                                      for i, row in enumerate(next_subboards[pos[2]][pos[3]])
-                                      for j, cell in enumerate(row)
-                                      if cell == EMPTY_CHAR)
-            else:
-                next_free_cells = set()
-                for i, row in enumerate(next_subboards):
-                    for j, board in enumerate(row):
-                        if next_board[i][j] != EMPTY_CHAR: continue
-                        next_free_cells.update((i, j , k, l)
-                                               for k, subrow in enumerate(board)
-                                               for l, cell in enumerate(subrow)
-                                               if cell == EMPTY_CHAR)
+            next_b2p = (pos[2], pos[3]) if next_board[pos[2]][pos[3]] == EMPTY_CHAR else None
 
 
             new_states.append(UltimateGameState(next_board, next_subboards,
                                                 next_play_char, self._max_player,
-                                                free_cells=next_free_cells,
+                                                board_to_play=next_b2p,
                                                 previous_move=pos))
         return new_states
+    
+    def heuristic_score(self) -> NumericType:
+        pass
