e1065a932a4dc154e2ef4f75298ddba6f22221df
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-01-23 18:26:40 +0700

    simulation step cause program to crash
    the simulation step can cause the program to crash sometimes when the chosen leaf node is a terminal node.
    
    To fix this, the terminal condition is checked before doing playout

--- bot/mcts.py
+++ bot/mcts.py
@@ -109,10 +109,16 @@ def _traversal(node: MonteCarloNode, kill_signal: list[bool]) -> MonteCarloNode:
 
 def _simulation(node: MonteCarloNode, kill_signal: list[bool]) -> PlayerCharacter | Literal['T']:
     """Simulation state in Monte Carlo Tree Search. Playout is selected randomly"""
-    state = rand.choice(node.playouts())
-    while not state.game_over and (not kill_signal[0]):
-        state = rand.choice(state.expand_state())
-    return 'T' if (winner := get_winner(state._board_state)) is None else winner
+    # if not node.is_terminal:
+    #     state = rand.choice(node.playouts())
+    #     while not state.game_over and (not kill_signal[0]):
+    #         state = rand.choice(state.expand_state())
+    # else: state = node._state
+
+    while not node.is_terminal and (not kill_signal[0]):
+        node = rand.choice(list(node.children))
+
+    return 'T' if (winner := get_winner(node._state._board_state)) is None else winner
 
 def _backpropogate(node: MonteCarloNode, result: PlayerCharacter | Literal['T'], kill_signal: list[bool]) -> None:
     while node and (not kill_signal[0]):
