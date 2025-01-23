5a4a1459bda8c93bc204683eaf0fd0fd917442f7
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-01-23 18:29:36 +0700

    Update mcts.py
    forgot about the comments lol

--- bot/mcts.py
+++ bot/mcts.py
@@ -109,12 +109,6 @@ def _traversal(node: MonteCarloNode, kill_signal: list[bool]) -> MonteCarloNode:
 
 def _simulation(node: MonteCarloNode, kill_signal: list[bool]) -> PlayerCharacter | Literal['T']:
     """Simulation state in Monte Carlo Tree Search. Playout is selected randomly"""
-    # if not node.is_terminal:
-    #     state = rand.choice(node.playouts())
-    #     while not state.game_over and (not kill_signal[0]):
-    #         state = rand.choice(state.expand_state())
-    # else: state = node._state
-
     while not node.is_terminal and (not kill_signal[0]):
         node = rand.choice(list(node.children))
 
