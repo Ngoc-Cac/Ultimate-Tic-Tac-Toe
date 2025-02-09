1480a9e9bdebfde2ff4888691c9ffd1ec559b792
Trịnh Ngọc Các <cactrinh5@gmail.com>
2025-02-09 14:38:23 +0700

    added exe for windows

--- src/log_setup.py
+++ src/log_setup.py
@@ -18,7 +18,7 @@ To setup, just import this file. Afterwards, call logging.getLogger(__name__)
 to create a logger and just log away.
 """
 
-ROOT_DIR = Path(osp.abspath(__file__)).parents[1]
+ROOT_DIR = osp.abspath(osp.dirname(__file__))
 log_dir = osp.join(ROOT_DIR, 'logs')
 if not osp.exists(log_dir):
     Path(log_dir).mkdir()
